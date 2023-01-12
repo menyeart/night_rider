12ba0494c916e2067021bdcff7a7a4f3fddb0f8c
Matt Enyeart <menyeart1@gmail.com>
2023-01-12 16:05:32 -0700

    Add a convert character method test

--- spec/braille_writer_spec.rb
+++ spec/braille_writer_spec.rb
@@ -1,5 +1,6 @@
 require './spec/spec_helper'
 require './lib/braille_writer'
+require './lib/dictionary'
 
 describe 'braillewriter' do
   before(:all) do
@@ -7,12 +8,16 @@ describe 'braillewriter' do
     input_file = '.message.txt'
     output_file = '.braille.txt'
     @braille_writer = BrailleWriter.new(input_file, output_file)
+    @dictionary = Dictionary.new
   end
 
   it "exists" do
     expect(@braille_writer).to be_an_instance_of(BrailleWriter)
   end
 
+  it "can convert a single character to it's dictionary class equivalent" do
+    expect(@braille_writer.convert_character('a')).to eq({ top: ["0","."], mid: ["0","0"], low: ["0","0"] })
+  end
 
 
 
