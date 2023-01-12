833f8e6848737dca9a74f19dfef9cfbbb66dc7ed
Matt Enyeart <menyeart1@gmail.com>
2023-01-12 16:06:45 -0700

    Add require for new dictionary class file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,10 +1,13 @@
 require './lib/braille_writer'
+require './lib/dictionary'
 
 input_file = File.open(ARGV[0], "r")
 output_file = File.new(ARGV[1], "w")
+dictionary = Dictionary.new
 braille_writer = BrailleWriter.new(input_file, output_file)
 braille_writer.run
 require 'pry'; binding.pry
+var = 0
 
 
 
