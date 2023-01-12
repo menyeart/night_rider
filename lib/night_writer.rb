1a19a0e8de4329810775dcc8e82a29821b605602
Matt Enyeart <menyeart1@gmail.com>
2023-01-12 15:27:21 -0700

    Add code to run the braille writer class, remove method for counting characters

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,4 +1,12 @@
-message_file = File.open(ARGV[0], "r")
-new_file = File.new(ARGV[1], "w")
-character_count = File.readlines("message.txt").join.length
-puts "Created 'braille.txt' containing #{character_count} characters"
+require './lib/braille_writer'
+
+input_file = File.open(ARGV[0], "r")
+output_file = File.new(ARGV[1], "w")
+braille_writer = BrailleWriter.new(input_file, output_file)
+braille_writer.run
+require 'pry'; binding.pry
+
+
+
+
+
