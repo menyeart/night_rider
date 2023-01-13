77b0aa1a93a402f808a39c028d2e17cc6e315c1c
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 14:57:35 -0700

    Split covert full message medthod into three sepearte methods for top, mid, low

--- lib/braille_writer.rb
+++ lib/braille_writer.rb
@@ -6,13 +6,15 @@ class BrailleWriter < Dictionary
   def initialize(input_file, output_file)
     @input_file = input_file
     @output_file = output_file
+    @blank_string = ''
   end
 
   def run
     @character_count = File.readlines(@input_file).join.length
     string_array = convert_to_strings_array(@input_file)
-    require 'pry'; binding.pry
-    convert_full_message(string_array)
+    convert_full_message_top(string_array)
+    convert_full_message_mid(string_array)
+    convert_full_message_low(string_array)
     puts "Created 'braille.txt' containing #{@character_count} characters"
   end
 
@@ -32,21 +34,29 @@ class BrailleWriter < Dictionary
     File.readlines(file).to_s.gsub(/[^0-9a-z ]/i, '').split("").join.split("")
   end
 
-  def convert_full_message(message)
+  def convert_full_message_top(message)
     message.each do |character|
-      puts convert_character_top(character)
-      putsconvert_character_top(character)
-      puts convert_character_top(character)
+      @blank_string.concat(convert_character_top(character)[0])
+      @blank_string.concat convert_character_top(character)[1]
     end
   end
 
+  def convert_full_message_mid(message)
+    message.each do |character|
+      @blank_string.concat convert_character_mid(character)[0]
+      @blank_string.concat convert_character_mid(character)[1]
+    end
+  end
 
+  def convert_full_message_low(message)
+    message.each do |character|
+      @blank_string.concat convert_character_low(character)[0]
+      @blank_string.concat convert_character_low(character)[1]
+    end
+    p @blank_string
+  end
 
-
-
-
-
-
+  
 
 
 end
