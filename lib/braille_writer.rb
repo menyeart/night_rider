4fc428c898bebd6435ffc4d285b0298f3a6b525a
Matt Enyeart <menyeart1@gmail.com>
2023-01-13 14:35:15 -0700

    Split convert character method into separate methods for top, mid, low

--- lib/braille_writer.rb
+++ lib/braille_writer.rb
@@ -10,16 +10,32 @@ class BrailleWriter < Dictionary
 
   def run
     @character_count = File.readlines(@input_file).join.length
-    convert_to_strings_array(@input_file)
+    string_array = convert_to_strings_array(@input_file)
+    require 'pry'; binding.pry
+    convert_full_message(string_array)
     puts "Created 'braille.txt' containing #{@character_count} characters"
   end
 
   def convert_character(character)
-    self.english_to_braille[character]
+    self.english_to_braille[character][:top]
+  end
+
+  def convert_character(character)
+    self.english_to_braille[character][:mid]
+  end
+
+  def convert_character(character)
+    self.english_to_braille[character][:low]
   end
 
   def convert_to_strings_array(file)
     File.readlines(file).to_s.gsub(/[^0-9a-z ]/i, '').split("").join.split("")
+  end
+
+  def convert_full_message(message)
+    message.each do |character|
+      puts convert_character(character)
+    end
   end
 
 
