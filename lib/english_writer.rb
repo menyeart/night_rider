a2a5430e93d0c1cb501dd58b6656bf403e9290f5
Matt Enyeart <menyeart1@gmail.com>
2023-01-15 12:16:49 -0700

    Add convert to eng char method

--- lib/english_writer.rb
+++ lib/english_writer.rb
@@ -1,12 +1,38 @@
 require './lib/dictionary'
 
-class EnglishWriter< Dictionary
+class EnglishWriter < Dictionary
   attr_reader :input_file, :output_file, :output_string
 
   def initialize(input_file, output_file)
     @input_file = input_file
     @output_file = output_file
     @output_string = ""
-    @character_count = File.readlines(@input_file).join.length
+    @character_count = File.readlines(@output_file).join.length
   end
 
+  def run
+    puts "Created original_message.txt containing #{@character_count} characters"
+  end
+
+  def create_line_array(file)
+    File.readlines(file).map do |line| 
+      line.delete("\n")
+    end
+  end
+
+  def build_braille_character_string(line_array)
+    braille_string = ''
+    line_array.each do |line|
+      braille_string.concat(line[0,2])
+      line.slice(0,2)
+    end
+    braille_string
+  end
+
+  def convert_to_eng_char(string)
+    self.braille_to_english[string]
+  end
+
+
+
+end
