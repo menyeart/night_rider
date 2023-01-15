4720b4e52bd3ca1a5e5209881184fcfd517d5f23
Matt Enyeart <menyeart1@gmail.com>
2023-01-15 13:33:43 -0700

    Add a test for the english to braille method hash

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -7,6 +7,14 @@ describe 'dictionary' do
     expect(dictionary).to be_an_instance_of(Dictionary)
   end
 
+  it "defines an alphabet of english to braille characters" do
+    dictionary = Dictionary.new
+    expect(dictionary.english_to_braille.keys.count).to eq(27)
+    expect(dictionary.english_to_braille.values.count).to eq(27)
+    expect(dictionary.english_to_braille["a"]).to eq({ top: ["0","."], mid: [".","."], low: [".","."] })
+  end
+
+
 
 
 end
