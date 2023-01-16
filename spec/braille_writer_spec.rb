ba2ef31861b22249ff7239814cb486be9aeed5ba
Matt Enyeart <menyeart1@gmail.com>
2023-01-16 09:45:17 -0700

    Change tests to reflect refactor and consolidation of convert char and concat full message methods

--- spec/braille_writer_spec.rb
+++ spec/braille_writer_spec.rb
@@ -17,38 +17,20 @@ describe 'braillewriter' do
     expect(@braille_writer).to be_an_instance_of(BrailleWriter)
   end
 
-  it "can convert a single character to it's dictionary class equivalent of the top part" do
-    expect(@braille_writer.convert_character_top('a')).to eq(["0","."])
-  end
-
-  it "can convert a single character to it's dictionary class equivalent of the middle part" do
-    expect(@braille_writer.convert_character_mid('a')).to eq([".", "."])
-  end
-
-  it "can convert a single character to it's dictionary class equivalent of the low part" do
-    expect(@braille_writer.convert_character_low('a')).to eq([".","."])
+  it "can convert a single character to it's dictionary class equivalent of the part" do
+    expect(@braille_writer.convert_character(:top, 'a')).to eq(["0","."])
+    expect(@braille_writer.convert_character(:mid, 'a')).to eq([".", "."])
+    expect(@braille_writer.convert_character(:low, 'a')).to eq([".", "."])
   end
 
   it "can convert an input file into a array of single character strings" do
     expect(@braille_writer.convert_to_strings_array(@input_file)).to eq(["a", "b", "c"])
   end
 
-  it "can concatenate every set of a message's converted top two braille characters into a string" do
+  it "can concatenate every set of a message's converted two braille characters into a string" do
     message = ["a","b","c"]
     @output_string = ''
-    expect(@braille_writer.concat_full_message_top(message, @output_string)).to eq("0.0.00")
-  end
-
-  it "can concatenate every set of a message's converted middle two braille characters into a string" do
-    message = ["a","b","c"]
-    @output_string = ''
-    expect(@braille_writer.concat_full_message_mid(message, @output_string)).to eq("..0...")
-  end
-
-  it "can concatenate every set of a message's converted low two braille characters into a string" do
-    message = ["a","b","c"]
-    @output_string = ''
-    expect(@braille_writer.concat_full_message_low(message, @output_string)).to eq("......")
+    expect(@braille_writer.concat_full_message(:top, message, @output_string)).to eq("0.0.00")
   end
 
   it "can insert newlines in the output string between the low, mid and top parts" do
